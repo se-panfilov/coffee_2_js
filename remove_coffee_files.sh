e957511c6f0200fb13ef10d93e6e8e66db9bbeba
Sergei <se-panfilov@ya.ru>
2017-08-07 20:44:56 +0800

    fixes for remove script

--- remove_coffee_files.sh
+++ remove_coffee_files.sh
@@ -6,11 +6,21 @@ NC='\033[0m' # No Color
 cd ./app/scripts/ &&
 
 printf "${GREEN}STEP 1: Remove COFFEE files${NC}\n";
-find ./ -name "*.coffee" -type f
+find ./ -name "*.coffee" -type f -delete
 printf "${GREEN}STEP 1: Remove COFFEE files... done${NC}\n";
 
 printf "${GREEN}STEP 2: Remove CJSX files${NC}\n";
-find ./ -name "*.cjsx" -type f
+find ./ -name "*.cjsx" -type f -delete
 printf "${GREEN}STEP 2: Remove CJSX files... done${NC}\n";
+
+cd ../../test
+
+printf "${GREEN}STEP 3: Remove COFFEE files... from tests...${NC}\n";
+find ./ -name "*.coffee" -type f -delete
+printf "${GREEN}STEP 3: Remove COFFEE files... from tests...done${NC}\n";
+
+printf "${GREEN}STEP 4: Remove CJSX files... from tests...${NC}\n";
+find ./ -name "*.cjsx" -type f -delete
+printf "${GREEN}STEP 4: Remove CJSX files... from tests... done${NC}\n";
 
 printf "${GREEN}Remove old files done!${NC}\n";
